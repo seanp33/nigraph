4c5e8de6eb0b51dd8ad6a6be6e25677f10f66fe7
Sean Monaghan <smonagan000@gmail.com>
2014-01-02 16:42:49 -0500

    initial commit

---
+++ binding.gyp
@@ -0,0 +1,10 @@
+{
+  "targets": [
+    {
+      "target_name": "node_igraph",
+      "sources": [ "node_igraph.cc" ],
+      "include_dirs": ["/usr/local/include/igraph"],
+      "libraries":['/usr/local/lib/libigraph.dylib',]
+    }
+  ]
+}
---
+++ build/Release/.deps/Release/obj.target/addon/addon.o.d
@@ -0,0 +1,159 @@
+cmd_Release/obj.target/addon/addon.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/smonaghan/.node-gyp/0.10.22/src -I/Users/smonaghan/.node-gyp/0.10.22/deps/uv/include -I/Users/smonaghan/.node-gyp/0.10.22/deps/v8/include -I/usr/local/include/igraph  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/addon/addon.o.d.raw  -c -o Release/obj.target/addon/addon.o ../addon.cc
+Release/obj.target/addon/addon.o: ../addon.cc \
+  /Users/smonaghan/.node-gyp/0.10.22/src/node.h \
+  /Users/smonaghan/.node-gyp/0.10.22/deps/uv/include/uv.h \
+  /Users/smonaghan/.node-gyp/0.10.22/deps/uv/include/uv-private/uv-unix.h \
+  /Users/smonaghan/.node-gyp/0.10.22/deps/uv/include/uv-private/ngx-queue.h \
+  /Users/smonaghan/.node-gyp/0.10.22/deps/uv/include/uv-private/uv-darwin.h \
+  /Users/smonaghan/.node-gyp/0.10.22/deps/v8/include/v8.h \
+  /Users/smonaghan/.node-gyp/0.10.22/deps/v8/include/v8stdint.h \
+  /Users/smonaghan/.node-gyp/0.10.22/src/node_object_wrap.h \
+  /usr/local/include/igraph/igraph.h \
+  /usr/local/include/igraph/igraph_version.h \
+  /usr/local/include/igraph/igraph_memory.h \
+  /usr/local/include/igraph/igraph_error.h \
+  /usr/local/include/igraph/igraph_random.h \
+  /usr/local/include/igraph/igraph_types.h \
+  /usr/local/include/igraph/igraph_progress.h \
+  /usr/local/include/igraph/igraph_statusbar.h \
+  /usr/local/include/igraph/igraph_complex.h \
+  /usr/local/include/igraph/igraph_vector.h \
+  /usr/local/include/igraph/igraph_pmt.h \
+  /usr/local/include/igraph/igraph_vector_pmt.h \
+  /usr/local/include/igraph/igraph_pmt_off.h \
+  /usr/local/include/igraph/igraph_matrix.h \
+  /usr/local/include/igraph/igraph_matrix_pmt.h \
+  /usr/local/include/igraph/igraph_array.h \
+  /usr/local/include/igraph/igraph_array_pmt.h \
+  /usr/local/include/igraph/igraph_dqueue.h \
+  /usr/local/include/igraph/igraph_dqueue_pmt.h \
+  /usr/local/include/igraph/igraph_stack.h \
+  /usr/local/include/igraph/igraph_stack_pmt.h \
+  /usr/local/include/igraph/igraph_heap.h \
+  /usr/local/include/igraph/igraph_heap_pmt.h \
+  /usr/local/include/igraph/igraph_psumtree.h \
+  /usr/local/include/igraph/igraph_strvector.h \
+  /usr/local/include/igraph/igraph_vector_ptr.h \
+  /usr/local/include/igraph/igraph_spmatrix.h \
+  /usr/local/include/igraph/igraph_sparsemat.h \
+  /usr/local/include/igraph/igraph_datatype.h \
+  /usr/local/include/igraph/igraph_arpack.h \
+  /usr/local/include/igraph/igraph_qsort.h \
+  /usr/local/include/igraph/igraph_constants.h \
+  /usr/local/include/igraph/igraph_iterators.h \
+  /usr/local/include/igraph/igraph_interface.h \
+  /usr/local/include/igraph/igraph_constructors.h \
+  /usr/local/include/igraph/igraph_games.h \
+  /usr/local/include/igraph/igraph_microscopic_update.h \
+  /usr/local/include/igraph/igraph_centrality.h \
+  /usr/local/include/igraph/igraph_paths.h \
+  /usr/local/include/igraph/igraph_components.h \
+  /usr/local/include/igraph/igraph_structural.h \
+  /usr/local/include/igraph/igraph_attributes.h \
+  /usr/local/include/igraph/igraph_transitivity.h \
+  /usr/local/include/igraph/igraph_neighborhood.h \
+  /usr/local/include/igraph/igraph_topology.h \
+  /usr/local/include/igraph/igraph_bipartite.h \
+  /usr/local/include/igraph/igraph_cliques.h \
+  /usr/local/include/igraph/igraph_layout.h \
+  /usr/local/include/igraph/igraph_visitor.h \
+  /usr/local/include/igraph/igraph_community.h \
+  /usr/local/include/igraph/igraph_conversion.h \
+  /usr/local/include/igraph/igraph_foreign.h \
+  /usr/local/include/igraph/igraph_motifs.h \
+  /usr/local/include/igraph/igraph_operators.h \
+  /usr/local/include/igraph/igraph_flow.h \
+  /usr/local/include/igraph/igraph_revolver.h \
+  /usr/local/include/igraph/igraph_adjlist.h \
+  /usr/local/include/igraph/igraph_nongraph.h \
+  /usr/local/include/igraph/igraph_cocitation.h \
+  /usr/local/include/igraph/igraph_blas.h \
+  /usr/local/include/igraph/igraph_lapack.h \
+  /usr/local/include/igraph/igraph_mixing.h \
+  /usr/local/include/igraph/igraph_separators.h \
+  /usr/local/include/igraph/igraph_cohesive_blocks.h \
+  /usr/local/include/igraph/igraph_eigen.h \
+  /usr/local/include/igraph/igraph_hrg.h \
+  /usr/local/include/igraph/igraph_threading.h \
+  /usr/local/include/igraph/igraph_interrupt.h \
+  /usr/local/include/igraph/igraph_scg.h \
+  /usr/local/include/igraph/igraph_matching.h
+../addon.cc:
+/Users/smonaghan/.node-gyp/0.10.22/src/node.h:
+/Users/smonaghan/.node-gyp/0.10.22/deps/uv/include/uv.h:
+/Users/smonaghan/.node-gyp/0.10.22/deps/uv/include/uv-private/uv-unix.h:
+/Users/smonaghan/.node-gyp/0.10.22/deps/uv/include/uv-private/ngx-queue.h:
+/Users/smonaghan/.node-gyp/0.10.22/deps/uv/include/uv-private/uv-darwin.h:
+/Users/smonaghan/.node-gyp/0.10.22/deps/v8/include/v8.h:
+/Users/smonaghan/.node-gyp/0.10.22/deps/v8/include/v8stdint.h:
+/Users/smonaghan/.node-gyp/0.10.22/src/node_object_wrap.h:
+/usr/local/include/igraph/igraph.h:
+/usr/local/include/igraph/igraph_version.h:
+/usr/local/include/igraph/igraph_memory.h:
+/usr/local/include/igraph/igraph_error.h:
+/usr/local/include/igraph/igraph_random.h:
+/usr/local/include/igraph/igraph_types.h:
+/usr/local/include/igraph/igraph_progress.h:
+/usr/local/include/igraph/igraph_statusbar.h:
+/usr/local/include/igraph/igraph_complex.h:
+/usr/local/include/igraph/igraph_vector.h:
+/usr/local/include/igraph/igraph_pmt.h:
+/usr/local/include/igraph/igraph_vector_pmt.h:
+/usr/local/include/igraph/igraph_pmt_off.h:
+/usr/local/include/igraph/igraph_matrix.h:
+/usr/local/include/igraph/igraph_matrix_pmt.h:
+/usr/local/include/igraph/igraph_array.h:
+/usr/local/include/igraph/igraph_array_pmt.h:
+/usr/local/include/igraph/igraph_dqueue.h:
+/usr/local/include/igraph/igraph_dqueue_pmt.h:
+/usr/local/include/igraph/igraph_stack.h:
+/usr/local/include/igraph/igraph_stack_pmt.h:
+/usr/local/include/igraph/igraph_heap.h:
+/usr/local/include/igraph/igraph_heap_pmt.h:
+/usr/local/include/igraph/igraph_psumtree.h:
+/usr/local/include/igraph/igraph_strvector.h:
+/usr/local/include/igraph/igraph_vector_ptr.h:
+/usr/local/include/igraph/igraph_spmatrix.h:
+/usr/local/include/igraph/igraph_sparsemat.h:
+/usr/local/include/igraph/igraph_datatype.h:
+/usr/local/include/igraph/igraph_arpack.h:
+/usr/local/include/igraph/igraph_qsort.h:
+/usr/local/include/igraph/igraph_constants.h:
+/usr/local/include/igraph/igraph_iterators.h:
+/usr/local/include/igraph/igraph_interface.h:
+/usr/local/include/igraph/igraph_constructors.h:
+/usr/local/include/igraph/igraph_games.h:
+/usr/local/include/igraph/igraph_microscopic_update.h:
+/usr/local/include/igraph/igraph_centrality.h:
+/usr/local/include/igraph/igraph_paths.h:
+/usr/local/include/igraph/igraph_components.h:
+/usr/local/include/igraph/igraph_structural.h:
+/usr/local/include/igraph/igraph_attributes.h:
+/usr/local/include/igraph/igraph_transitivity.h:
+/usr/local/include/igraph/igraph_neighborhood.h:
+/usr/local/include/igraph/igraph_topology.h:
+/usr/local/include/igraph/igraph_bipartite.h:
+/usr/local/include/igraph/igraph_cliques.h:
+/usr/local/include/igraph/igraph_layout.h:
+/usr/local/include/igraph/igraph_visitor.h:
+/usr/local/include/igraph/igraph_community.h:
+/usr/local/include/igraph/igraph_conversion.h:
+/usr/local/include/igraph/igraph_foreign.h:
+/usr/local/include/igraph/igraph_motifs.h:
+/usr/local/include/igraph/igraph_operators.h:
+/usr/local/include/igraph/igraph_flow.h:
+/usr/local/include/igraph/igraph_revolver.h:
+/usr/local/include/igraph/igraph_adjlist.h:
+/usr/local/include/igraph/igraph_nongraph.h:
+/usr/local/include/igraph/igraph_cocitation.h:
+/usr/local/include/igraph/igraph_blas.h:
+/usr/local/include/igraph/igraph_lapack.h:
+/usr/local/include/igraph/igraph_mixing.h:
+/usr/local/include/igraph/igraph_separators.h:
+/usr/local/include/igraph/igraph_cohesive_blocks.h:
+/usr/local/include/igraph/igraph_eigen.h:
+/usr/local/include/igraph/igraph_hrg.h:
+/usr/local/include/igraph/igraph_threading.h:
+/usr/local/include/igraph/igraph_interrupt.h:
+/usr/local/include/igraph/igraph_scg.h:
+/usr/local/include/igraph/igraph_matching.h:
