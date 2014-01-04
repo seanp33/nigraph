473c6cb147c57b9f52ea7e17ed6959e8c871a903
Sean Monaghan <smonagan000@gmail.com>
2014-01-04 00:40:03 -0500

    learning more v8 and igraph. sending out js object with members, applying sphere layout to game and printf'ng coords

--- nigraph.cc
+++ nigraph.cc
@@ -5,42 +5,81 @@
 
 using namespace v8;
 
-Handle<Value> RunCallback(const Arguments& args) {
+Handle<Value> run_igraph_erdos_renyi_game(const Arguments& args) {
   HandleScope scope;
-
-  Local<Function> cb = Local<Function>::Cast(args[0]);
-  const unsigned argc = 1;
-  Local<Value> argv[argc] = { Local<Value>::New(String::New("hello world")) };
-  cb->Call(Context::GetCurrent()->Global(), argc, argv);
-
-  return scope.Close(Undefined());
-}
-
-Handle<Value> RunIGraphCallback(const Arguments& args) {
   igraph_integer_t diameter;
   igraph_t graph;
-  igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP, 1000, 5.0/1000,
+  igraph_matrix_t coords;
+  igraph_vs_t vtx_selector;
+  igraph_vit_t vtx_iter;
+
+  //turn on attribute handling - http://igraph.sourceforge.net/doc/html/ch12s02.html
+  //igraph_i_set_attribute_table(&igraph_cattribute_table);
+
+  Local<Number> vertex_count_in = Local<Number>::Cast(args[0]);
+
+  igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP, vertex_count_in->Value(), 5.0/1000,
 			  IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
+
   igraph_diameter(&graph, &diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);  
 
-  HandleScope scope;
-  Local<Function> cb = Local<Function>::Cast(args[0]);
+  // layout
+  igraph_matrix_init(&coords, 0, 0);
+  igraph_layout_sphere(&graph, &coords);
+  //igraph_matrix_print(&coords); 
+  
+  // and my attempt at getting coords based off of igraph_matrix_print impl .. AND IT WORKS!!
+  long int nr = 10;
+  long int nc = 3;
+  long int i, j;
+  for (i=0; i<nr; i++) {
+    for (j=0; j<nc; j++) {
+      if (j!=0) { putchar(' '); }
+      printf("%f", MATRIX(coords, i, j));
+    }
+    printf("\n");
+  }
+
+  //print_attributes(&graph);
+
+  // selectors and iterators
+  igraph_vs_seq(&vtx_selector, 0, (vertex_count_in->Value() -1));
+  igraph_vit_create(&graph, vtx_selector, &vtx_iter);
+
+
+  while (!IGRAPH_VIT_END(vtx_iter)) {
+    //printf(" %li", (long int)IGRAPH_VIT_GET(vtx_iter));
+    IGRAPH_VIT_NEXT(vtx_iter);
+  }
+ 
+  // and perform callback
+  Local<Function> cb = Local<Function>::Cast(args[1]);
   const unsigned argc = 1;
 
-  Local<Value> v_count = Number::New(igraph_vcount(&graph));
-  Local<Value> diam  = Number::New(diameter);
-  Local<Value> diam_str = String::Cast(* diam);
-  // Local<Value> argv[argc] = { Local<Value>::New(diam_str) };
-   Local<Value> argv[argc] = { v_count};
+  Local<Value> v_count_out = Number::New(igraph_vcount(&graph));
+  Local<Value> e_count_out = Number::New(igraph_ecount(&graph));
+  Local<Value> diameter_out = Number::New(diameter);
+  Local<Object> result = Object::New();
+
+  result->Set(String::NewSymbol("v_count"), v_count_out);
+  result->Set(String::NewSymbol("e_count"), e_count_out);
+  result->Set(String::NewSymbol("diameter"), diameter_out);
+
+  Local<Value> argv[argc] = {result};
+
   cb->Call(Context::GetCurrent()->Global(), argc, argv);
 
   igraph_destroy(&graph);
+  igraph_matrix_destroy(&coords);
+  igraph_vit_destroy(&vtx_iter);
+  igraph_vs_destroy(&vtx_selector);
+
   return scope.Close(Undefined());
 }
 
 void Init(Handle<Object> exports, Handle<Object> module) {
-  module->Set(String::NewSymbol("exports"),
-      FunctionTemplate::New(RunIGraphCallback)->GetFunction());
+   exports->Set(String::NewSymbol("igraph_erdos_renyi_game"),
+       FunctionTemplate::New(run_igraph_erdos_renyi_game)->GetFunction());
 }
 
 NODE_MODULE(nigraph, Init)
--- smoke.js
+++ smoke.js
@@ -1,5 +1,7 @@
 var nigraph = require('./build/Release/nigraph');
 
-nigraph(function(msg){
-  console.log(msg);
+nigraph.igraph_erdos_renyi_game(100, function(result){
+  console.log("v_count: " + result.v_count);
+  console.log("e_count: " + result.e_count);
+  console.log("diameter: " + result.diameter);
 });
