1e15e8343c0fe52f31eb2f5c9c863c74f4f140d6
smonaghan <smonaghan@eitccorp.com>
2016-07-16 16:06:09 -0400

    qualifying v8 as I learn, readme

--- src/main/cpp/nigraph2.cc
+++ src/main/cpp/nigraph2.cc
@@ -8,25 +8,25 @@ void igraph_erdos_renyi_game(const Nan::FunctionCallbackInfo<Value>& info){
   igraph_t g;
   igraph_matrix_t coords;
 
-  Local<Number> vertex_count_in = info[0].As<Number>();
-  Local<Function> cb = info[1].As<Function>();
+  v8::Local<v8::Number> vertex_count_in = info[0].As<v8::Number>();
+  v8::Local<v8::Function> cb = info[1].As<v8::Function>();
 
   igraph_erdos_renyi_game(&g, IGRAPH_ERDOS_RENYI_GNP, vertex_count_in->Value(), 5.0/vertex_count_in->Value(), IGRAPH_DIRECTED, IGRAPH_NO_LOOPS);
 
   // counts
-  Local<Value> v_count_out = Nan::New(igraph_vcount(&g));
-  Local<Value> e_count_out = Nan::New(igraph_ecount(&g));
+  v8::Local<v8::Number> v_count_out = Nan::New(igraph_vcount(&g));
+  v8::Local<v8::Number> e_count_out = Nan::New(igraph_ecount(&g));
 
   // layout
   igraph_matrix_init(&coords, 0, 0);
   igraph_layout_sphere(&g, &coords);
 
   // collect coordinates from layout
-  Local<Array> points_array = Nan::New<Array>(vertex_count_in->Value());
+  v8::Local<v8::Array> points_array = Nan::New<v8::Array>(vertex_count_in->Value());
   long int nr = vertex_count_in->Value();
   long int i;
   for (i=0; i<nr; i++) {
-    Local<Array> points = Nan::New<Array>(3);
+    v8::Local<v8::Array> points = Nan::New<v8::Array>(3);
     points->Set(0, Nan::New(MATRIX(coords, i, 0)));
     points->Set(1, Nan::New(MATRIX(coords, i, 1)));
     points->Set(2, Nan::New(MATRIX(coords, i, 2)));
@@ -39,7 +39,7 @@ void igraph_erdos_renyi_game(const Nan::FunctionCallbackInfo<Value>& info){
   result->Set(Nan::New("points").ToLocalChecked(), points_array);
 
   const unsigned argc = 1;
-  Local<Value> argv[argc] = {result};
+  v8::Local<v8::Value> argv[argc] = {result};
 
   igraph_destroy(&g);
   igraph_matrix_destroy(&coords);
@@ -47,9 +47,9 @@ void igraph_erdos_renyi_game(const Nan::FunctionCallbackInfo<Value>& info){
   Nan::MakeCallback(Nan::GetCurrentContext()->Global(), cb, argc, argv);
 }
 
-void InitAll(Local<Object> exports) {
+void InitAll(v8::Local<v8::Object> exports) {
   exports->Set(Nan::New("igraph_erdos_renyi_game").ToLocalChecked(),
-      Nan::New<FunctionTemplate>(igraph_erdos_renyi_game)->GetFunction());
+      Nan::New<v8::FunctionTemplate>(igraph_erdos_renyi_game)->GetFunction());
 
 //  exports->Set(Nan::New("add").ToLocalChecked(),
 //      Nan::New<FunctionTemplate>(Add)->GetFunction());
